78a2abb400ce708da5a169b2b871ff80417acfb9
Danagul Akpaeva <aidanagul10@gmail.com>
2019-03-14 22:51:21 +0300

    Добавьте файлы проекта.

---
+++ NA_laba2/Matrix.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NA_laba2
+{
+    class Matrix
+    {
+        /// <summary>
+        /// Вывод матрицы
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        public static void Output(double[,] A, int n,int m)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                for(int j=0;j<m;j++)
+                    Console.Write(A[i,j]+" ");
+                Console.WriteLine();
+            }
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        /// <summary>
+        /// Вывод вектора
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        public static void Output(double[] A, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+            Console.WriteLine("----------------------------------------------------------");
+        }
+        /// <summary>
+        /// умножение матриц
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="k"></param>
+        public static double[,] MulMatrix(double[,] A, double[,] B, int n, int m, int k)
+        {
+            double[,] C = new double[n, k];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    for (int c = 0; c < m; c++)
+                    {
+                        C[i, j] += A[i, c] * B[c, j];
+                    }
+                }
+            }
+            Output(C, n, k);
+            return C;
+        }
+        /// <summary>
+        /// Умножение матрицы на вектор
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <param name="k"></param>
+        public static double[] MulMatrix(double[,] A, double[] B, int n, int m)
+        {
+            double[] C = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int c = 0; c < m; c++)
+                {
+                    C[i] += A[i, c] * B[c];
+                }
+            }
+            Output(C, n);
+            return C;
+        }/// <summary>
+         /// Ошибка матриц
+         /// </summary>
+         /// <param name="A"></param>
+         /// <param name="n"></param>
+         /// <param name="b"></param>
+        public static void Error(double[,] A, int n, double b)
+        {
+            double myB = average(A, n);
+            Console.WriteLine(" B = " + b);
+            Console.WriteLine("new B = " + myB);
+            Console.WriteLine("inccuracy = " + Math.Abs(b - myB) / b);
+            // Console.WriteLine("inccuracy = " + Math.Abs(b - myB) / myB);
+            Console.WriteLine("------------------------------------------------------------------");
+        }
+        /// <summary>
+        /// Ошибка вектора
+        /// </summary>
+        /// <param name="B"></param>
+        /// <param name="B_1"></param>
+        /// <param name="n"></param>
+        public static void Error(double[] B, double[] B_1, int n)
+        {
+            double b = average(B, n);
+            double new_b = average(B_1, n);
+            double incur = Math.Abs(b - new_b) / b;
+            Console.WriteLine("inccuracy = " + incur);
+        }
+        /// <summary>
+        /// Среднее квадратичное вектора
+        /// </summary>
+        /// <param name="B"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double average(double[] B, int n)
+        {
+            double delta = 0;
+            for (int i = 0; i < n; i++)
+                delta += B[i] * B[i];
+            return delta = Math.Sqrt(delta);
+        }
+        /// <summary>
+        /// Среднее квадратичное матрицы
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double average(double[,] A, int n)
+        {
+            double delta = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    delta += A[i, j] * A[i, j];
+            }
+
+            delta = Math.Sqrt(delta);
+            return delta;
+        } /// <summary>
+          /// Из квадратной матрицы А получаем прямоугольную путем добавления столбца В
+          /// </summary>
+          /// <param name="A"></param>
+          /// <param name="B"></param>
+          /// <param name="n"></param>
+          /// <returns></returns>
+        public static double[,] ExtendedMatrix(double[,] A, double[] B, int n)
+        {
+            double[,] newA = new double[n, n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n + 1; j++)
+                {
+                    if (j == n) newA[i, j] = B[i];
+                    else newA[i, j] = A[i, j];
+                }
+            }
+            return newA;
+        }
+        /// <summary>
+        /// Получение столбца В из расширенной матрицы
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="n"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        public static double[] GetBFromExtendedMatrix(double[,] A, int n, int m)
+        {
+            double[] B = new double[n];
+            for (int i = 0; i < n; i++)
+                B[i] = A[i, m - 1];
+            return B;
+        }
+    }
+}
---
+++ NA_laba2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NA_laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            //double[,] A1 = new double[n, n] { { 2, 1, 0, 0, 0 }, { 3, 7, 3, 0, 0 }, { 0, 2, 6, 3, 0 }, { 0, 0, 1, 2, 1 }, { 0, 0, 0, 1, 2 } };
+            //double[] d = new double[n] { 3, 13, 11, 4, 3 };
+            double[,] A1 = new double[n, n] { { -0.871, -0.44, 0, 0, 0 }, { -0.823, 0.718, -0.016, 0, 0 }, { 0, 0.684, 1.885, 0.01, 0 }, { 0, 0, 1.778, -0.172, 0.6 }, { 0, 0, 0, -0.099, 0.416 } };
+            double[] d = new double[n] { 0.628, 1.257, 1.885, 2.513, 3.142 };
+            double[,] A = Matrix.ExtendedMatrix(A1, d, n);
+
+            Matrix.Output(A1, n,n);
+            Console.WriteLine("---------------------------------------------");
+            double[] a = new double[n];
+            double[] b = new double[n];
+            double[] c = new double[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                if (i == 0) a[i] = 0;
+                else a[i] = A[i, i - 1];
+
+                if (i == n - 1) c[i] = 0;
+                else c[i] = A[i, i + 1];
+
+                b[i] = A[i, i];
+            }
+            //Matrix.Output(A1, n, n);
+            //Matrix.Output(a, n);
+            //Matrix.Output(b, n);
+            //Matrix.Output(c, n);
+
+            double[] alfa = new double[n];
+            double[] beta = new double[n];
+            alfa[0] = -(c[0] / b[0]);
+            beta[0] = d[0] / b[0];
+
+            for(int i = 1; i < n; i++)
+            {
+                alfa[i] = -(c[i] / (a[i] * alfa[i - 1] + b[i]));
+                beta[i] = (d[i] - a[i] * beta[i - 1]) / (a[i] * alfa[i - 1] + b[i]);
+            }
+            Console.WriteLine("-----alfa--------");
+            Matrix.Output(alfa, n);
+            Console.WriteLine("-----beta--------");
+            Matrix.Output(beta, n);
+
+            double[] x = new double[n];
+            x[n - 1] = (d[n - 1] - a[n - 1] * beta[n - 2]) / (a[n - 1] * alfa[n - 2] + b[n - 1]);
+            for(int i = n - 2; i >= 0; i--)
+            {
+                x[i] = alfa[i] * x[i + 1] + beta[i];
+            }
+            Console.WriteLine("-----X--------");
+            Matrix.Output(x, n);
+
+            Console.WriteLine("-----Error-------");
+            Matrix.Error(d, Matrix.MulMatrix(A1, x, n, n), n);
+
+            Console.ReadLine();
+        }
+    }
+}
